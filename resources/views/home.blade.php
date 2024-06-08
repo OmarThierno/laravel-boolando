53bf97558eadc6bbb35c59576b2e8c06757ce153
OmarThierno <thiernomarndiaye76@gmail.com>
2024-06-08 16:56:44 +0200

    feat: add main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
+    <main class="main-home">
+        <div class="container">
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col">
+                        <div class="card">
+                            <div class="img-container">
+                                <img class="front" src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}"
+                                    alt="">
+                                <img class="back" src="{{ Vite::asset('resources/img/' . $product['backImage']) }}"
+                                    alt="">
+
+                                <div class="badges">
+                                    @foreach ($product['badges'] as $badge)
+                                        <span
+                                            class="badge {{ $badge['type'] === 'tag' ? 'green' : 'red' }}">{{ $badge['value'] }}</span>
+                                    @endforeach
+                                </div>
+
+                            </div>
+                            <div class="card-body">
+                                <span class="brand">{{ $product['brand'] }}</span>
+                                <h3>{{ $product['name'] }}</h3>
+
+                                @foreach ($product['badges'] as $badge)
+                                    @if ($badge['type'] === 'discount')
+                                        <span
+                                            class="color-red">{{ round($product['price'] - ($product['price'] * substr($badge['value'], 1, -1)) / 100, 2) }}
+                                            €</span>
+                                    @endif
+                                @endforeach
+                                @if ($loop->last)
+                                  <span>{{ $product['price'] }} €</span>
+                                @else
+                                  <span class="line-through">{{ $product['price'] }} €</span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
 @endsection
